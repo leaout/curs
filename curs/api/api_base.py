bcff3103b61d4c0cead0cd7355d4ebea83e78bd7
chenly <501305528@qq.com>
2020-02-09 11:28:01 +0800

    update

--- curs/api/api_base.py
+++ curs/api/api_base.py
@@ -2,6 +2,7 @@ from curs.real_quote import *
 from curs.cursglobal import *
 from curs.const import *
 
+
 def all_instruments(type=None, date=None):
     '''
 
@@ -34,14 +35,15 @@ def get_period_int(frequency):
             return 60 * 60
     else:
         if frequency[0] == "5":
-            return 5 * (24*60*60)
+            return 5 * (24 * 60 * 60)
         elif frequency[0] == "15":
-            return 15 * (24*60*60)
+            return 15 * (24 * 60 * 60)
         elif frequency[0] == "30":
-            return 30 * (24*60*60)
+            return 30 * (24 * 60 * 60)
         elif frequency[0] == "60":
-            return 60 * (24*60*60)
+            return 60 * (24 * 60 * 60)
     return 1
+
 
 def judge_period_border(unix_time):
     time_local = time.localtime(unix_time)
@@ -49,6 +51,7 @@ def judge_period_border(unix_time):
     if time_str == "15:00:00":
         return False
     return True
+
 
 def history_bars(order_book_id, bar_count, frequency="1m", fields=None):
     '''
@@ -59,77 +62,76 @@ def history_bars(order_book_id, bar_count, frequency="1m", fields=None):
     :param fields:
     :return:
     '''
-    try:
-        buddle_manager = None
-        gl = CursGlobal.get_instance()
-        ismin = False
+    # try:
+    buddle_manager = None
+    gl = CursGlobal.get_instance()
+    ismin = False
 
-        if frequency[1] == "m" or frequency[1] == "M":
-            buddle_manager = gl.min_buddles
-            ismin = True
-        else:
-            buddle_manager = gl.day_buddles
+    if frequency[1] == "m" or frequency[1] == "M":
+        buddle_manager = gl.min_buddles
+        ismin = True
+    else:
+        buddle_manager = gl.day_buddles
 
-        buddle = buddle_manager.get_buddle(order_book_id)
-        if buddle is None:
-            return None
-        if order_book_id not in gl.stock_map.keys():
-            return None
-        #计算bar_count
-        bar_count = int(frequency[0])*bar_count
-
-        np_arr = None
-        if ismin:
-            real_min = gl.stock_map[order_book_id]["1m"]
-            real_len = 0
-            if real_min is not None:
-                real_len = len(real_min)
-            sub_counts = bar_count - real_len
-            if sub_counts == 0:
-                np_arr = real_min
-            elif sub_counts < 0:
-                #只从当天分钟线中取数据
-                np_arr = real_min[(real_len - bar_count):]
-            else:
-                sub_counts += 1
-                # print(len(buddle[-sub_counts:-1]))
-                if real_min is not None:
-                    np_arr = np.concatenate([buddle[-sub_counts:-1], real_min])
-                else:
-                    np_arr = buddle[-sub_counts:-1]
-        else:
-            if bar_count >= len(buddle):
-                np_arr = buddle[:]
-            else:
-                np_arr = buddle[-bar_count:-1]
-        # print(len(np_arr))
-        ret_df = pd.DataFrame(np_arr)
-        ret_df.columns = pd_names
-        #同一周期时间 改为相同时间
-        period_time = get_period_int(frequency)
-        ret_df['time'] = ret_df['time'].map(lambda s: ((s +(period_time - s % period_time)) if judge_period_border(
-            s) else s))
-
-        ret_df['time'] = ret_df['time'].map(unix_to_timestamp)
-        ret_df = ret_df.set_index("time")
-        #index 需要转成 datetime 类型
-        ret_df.index = pd.to_datetime(ret_df.index)
-        #TODO 15:00 无法归并到前一分钟  Done
-        ret_df = ret_df.groupby("time").agg({'open': lambda s: s[0],
-                                             'close': lambda s: s[-1],
-                                             'high': lambda s: s.max(),
-                                             'low': lambda s: s.min(),
-                                             'volume': lambda s: s.sum(),
-                                             'money': lambda s: s.sum()})
-
-        # ret_df.dropna(axis=0, how='any', inplace=True)
-        return ret_df
-    except Exception as e:
-        logger.error(e)
+    buddle = buddle_manager.get_buddle(order_book_id)
+    if buddle is None:
         return None
+    if order_book_id not in gl.stock_map.keys():
+        return None
+    # 计算bar_count
+    bar_count = int(frequency[0]) * bar_count
 
+    np_arr = None
+    if ismin:
+        real_min = gl.stock_map[order_book_id]["1m"]
+        real_len = 0
+        if real_min is not None:
+            real_len = len(real_min)
+        sub_counts = bar_count - real_len
+        if sub_counts == 0:
+            np_arr = real_min
+        elif sub_counts < 0:
+            # 只从当天分钟线中取数据
+            np_arr = real_min[(real_len - bar_count):]
+        else:
+            sub_counts += 1
+            # print(len(buddle[-sub_counts:-1]))
+            if real_min is not None:
+                np_arr = np.concatenate([buddle[-sub_counts:-1], real_min])
+            else:
+                np_arr = buddle[-sub_counts:-1]
+    else:
+        if bar_count >= len(buddle):
+            np_arr = buddle[:]
+        else:
+            np_arr = buddle[-bar_count:-1]
+    # print(len(np_arr))
+    ret_df = pd.DataFrame(np_arr)
+    ret_df.columns = pd_names
+    # 同一周期时间 改为相同时间
+    period_time = get_period_int(frequency)
+    ret_df['time'] = ret_df['time'].map(lambda s: ((s + (period_time - s % period_time)) if judge_period_border(
+        s) else s))
 
+    ret_df['time'] = ret_df['time'].map(unix_to_timestamp)
+    ret_df = ret_df.set_index("time")
+    # print(ret_df)
+    # index 需要转成 datetime 类型
+    ret_df.index = pd.to_datetime(ret_df.index)
+    # TODO 15:00 无法归并到前一分钟  Done
+    # print(ret_df)
+    ret_df = ret_df.groupby('time').agg({'open': lambda s: s[0],
+                                         'close': lambda s: s[-1],
+                                         'high': lambda s: s.max(),
+                                         'low': lambda s: s.min(),
+                                         'volume': lambda s: s.sum(),
+                                         'money': lambda s: s.sum()})
 
+    # ret_df.dropna(axis=0, how='any', inplace=True)
+    return ret_df
+# except Exception as e:
+#     logger.error(e)
+#     return None
 
 
 def subscribe_min(order_book_id):
--- test/curs_test.py
+++ test/curs_test.py
@@ -3,7 +3,8 @@ from curs.real_quote import *
 from curs.data_source import *
 from curs.utils import *
 import tushare as ts
-
+import csv
+import pandas as pd
 x = 10
 def test_eval():
     y = 20  # 局部变量y
@@ -12,16 +13,76 @@ def test_eval():
 
 def ts_to_csv():
     stock_data = ts.get_stock_basics()
-    stock_data.to_csv('stock.csv')
+    # stock_data.to_csv('stock.csv',columns=['name'])
 
+    stock_edges = pd.DataFrame(columns=['fromID', 'toID', 'type'])
+
+    block_nodes = {}
+    begin_code = int(900001);
+    # industry
     block_industry = ts.get_industry_classified()
-    block_industry.to_csv('block_industry.csv')
+    for row in block_industry.iterrows():
+        if row[1]['c_name'] not in block_nodes:
+            block_nodes[row[1]['c_name']] = begin_code
+            begin_code += 1
+        stock_edges = stock_edges.append({'fromID': row[1]['code'], 'toID': str(block_nodes[row[1]['c_name']]), 'type': '属于'},
+                                             ignore_index=True)
 
+    # block_industry.to_csv('block_industry.csv',index=None)
+    # concept
     block_concept = ts.get_concept_classified()
-    block_concept.to_csv('block_concept.csv')
+    for row in block_concept.iterrows():
+        if row[1]['c_name'] not in block_nodes:
+            block_nodes[row[1]['c_name']] = begin_code
+            begin_code += 1
+        stock_edges = stock_edges.append({'fromID': row[1]['code'], 'toID': str(block_nodes[row[1]['c_name']]), 'type': '属于'}, ignore_index=True)
 
+    # block_concept.to_csv('block_concept.csv',index=None)
+
+    # area
     block_area =ts.get_area_classified()
-    block_area.to_csv('block_area.csv')
+    for row in block_area.iterrows():
+        if row[1]['area'] not in block_nodes:
+            block_nodes[row[1]['area']] = begin_code
+            begin_code += 1
+
+        stock_edges = stock_edges.append({'fromID': row[1]['code'], 'toID': str(block_nodes[row[1]['area']]), 'type': '属于'}, ignore_index=True)
+
+
+    # block_area.to_csv('block_area.csv',index=None)
+    # print(stock_edges)
+    stock_edges.to_csv('stock_edges.csv',index=None, encoding="utf-8");
+    #to csv
+    # 1. 创建文件对象
+    block_df = pd.DataFrame(columns=['code', 'name'])
+    for k,v in block_nodes.items():
+        # print(k)
+        block_df = block_df.append({'code': str(v), 'name': k}, ignore_index=True)
+
+
+    block_df = block_df.set_index('code')
+    block_df = block_df.append(stock_data)
+    block_df.to_csv('stock_nodes.csv',columns=['name'], encoding="utf-8")
+
+def convert_gbk2utf8(file_name):
+
+    gbk_dat = open(file_name, "r", encoding="gbk")
+    utf8_dat = open(file_name+".utf8", "w", encoding="utf-8")
+    for line in gbk_dat.readlines():
+        utf8_dat.write(line)
+
+    utf8_dat.close()
+    gbk_dat.close()
+
+def gbk_to_utf8(source, target):
+    with open(source, "r", encoding="gbk") as src:
+        with open(target, "w", encoding="utf-8") as dst:
+            for line in src.readlines():
+                dst.write(line)
+
+def block_to_graph():
+
+    pass
 def main():
     #
     # carr = bcolz.carray(rootdir="E:/buddles/min/000001.XSHE",  mode="a")
