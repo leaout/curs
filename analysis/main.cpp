329fa75fc6daf5d86c065774acd4599d2bbab736
chenliangyu <chenliangyu@ultipa.com>
2023-11-19 19:46:58 +0800

    update

--- analysis/main.cpp
+++ analysis/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
+
+struct Buddle{
+    uint32_t time;
+    uint32_t open;
+    uint32_t close;
+    uint32_t high;
+    uint32_t low;
+    uint32_t volume;
+    uint64_t money;
+};
+class MemoryBuddle{
+public:
+    enum class Period{
+        MIN1 = 0,
+        MIN5 ,
+        MIN15 ,
+        MIN30 ,
+        MIN60 ,
+        DAY ,
+        WEEK ,
+        MONTH,
+        SEASON,
+        YEAR,
+    };
+private:
+    map<pair<uint64_t,Period>,vector<Buddle>> m_data;
+};
+class DataManager{
+public:
+    void buddle_loader(){
+
+    }
+};
 
 int main() {
     std::cout << "hello quant!" << std::endl;
