1105acb558fed7cf81091b40b716e3c81a20446f
chenliangyu <chenliangyu@ultipa.com>
2023-11-21 20:54:04 +0800

    update

--- analysis/main.cpp
+++ analysis/main.cpp
@@ -14,21 +14,21 @@ struct Buddle {
     uint32_t volume;
     uint64_t money;
 };
-
+enum class Period {
+    MIN1 = 0,
+    MIN5,
+    MIN15,
+    MIN30,
+    MIN60,
+    DAY,
+    WEEK,
+    MONTH,
+    SEASON,
+    YEAR,
+};
 class MemoryBuddle {
 public:
-    enum class Period {
-        MIN1 = 0,
-        MIN5,
-        MIN15,
-        MIN30,
-        MIN60,
-        DAY,
-        WEEK,
-        MONTH,
-        SEASON,
-        YEAR,
-    };
+
 private:
     map<pair<uint64_t, Period>, vector<Buddle>> m_data;
 };
@@ -41,9 +41,41 @@ public:
     }
 };
 
+struct Order{
+    enum class OrderType{
+        None,
+        Buy,
+        Sell
+    };
+    OrderType order_type;
+    uint64_t order_id;
+    uint64_t volume;
+    uint64_t amt;
+    double price = 0;
+};
+struct IndicatorOut{
+    vector<double> outs;
+    Order order;
+};
+class Indicator {
+protected:
+    DataManager *m_data_mgr;
+public:
+    Indicator(DataManager *intor) : m_data_mgr(intor) {
+
+    }
+    virtual size_t OutParamCount() = 0;
+    virtual void get_indicator(vector<IndicatorOut>& intor_outs) = 0;
+};
+
+class Engine{
+public:
+
+};
+
 int main() {
     shared_ptr<BaseSeoratorParser::BaseDriver> m_base_driver;
-    m_base_driver = make_shared<BaseSeoratorParser::BaseDriver>("\t", "600519.txt", 10000, 0, 0, -1);
+    m_base_driver = make_shared<BaseSeoratorParser::BaseDriver>("\t", "600519.txt", 100, 0, 0, -1);
     vector<pair<size_t,Row>> data;
     while(m_base_driver->next(data)){
         std::cout << "row size:" << data.size() << std::endl;
