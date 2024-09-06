c2c645c00cc7eb4410d88d19727824732d19c620
chenly <501305528@qq.com>
2024-09-06 17:20:13 +0800

    remove analysis

--- analysis/csvparser/generate.sh
+++
@@ -1,4 +0,0 @@
-PARSER_PATH=$PWD/
-
-bison -d -o  ${PARSER_PATH}SepratorParser.cpp --defines=${PARSER_PATH}SepratorParser.h ${PARSER_PATH}csv_seprator.yy -v
-flex -o ${PARSER_PATH}SepratorScanner.cpp ${PARSER_PATH}csv_seprator.ll
