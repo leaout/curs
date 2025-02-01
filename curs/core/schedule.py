1a51d98ee15fbfeb8aecd62ca061b83c23e12b26
Your Name <501305528@qq.com>
2025-02-01 18:59:45 +0800

    add test data

--- curs/core/schedule.py
+++ curs/core/schedule.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import threading
 from curs.events import *
+from curs.log_handler.logger import logger
 
 class EventsScheduler:
     def __init__(self, event_bus):
@@ -16,14 +17,14 @@ class EventsScheduler:
         schedule.every().day.at("15:15").do(self.after_trading)
 
     def before_trading(self):
-        print(f"{datetime.now()}: {EVENT.BEFORE_TRADING} 触发")
+        logger.info(f"{datetime.now()}: {EVENT.BEFORE_TRADING} 触发")
         # 在这里添加策略的预处理逻辑
-        self.event_bus.publish(EVENT.BEFORE_TRADING)
+        self.event_bus.put_event(Event(EVENT.BEFORE_TRADING))
 
     def after_trading(self):
-        print(f"{datetime.now()}: {EVENT.AFTER_TRADING} 触发")
+        logger.info(f"{datetime.now()}: {EVENT.AFTER_TRADING} 触发")
         # 在这里添加策略的后处理逻辑
-        self.event_bus.publish(EVENT.AFTER_TRADING)
+        self.event_bus.put_event(Event(EVENT.AFTER_TRADING))
 
     def run(self):
         while True:
